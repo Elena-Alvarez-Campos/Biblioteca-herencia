02618f44dc9d3d0eca4059e0fcc506e229054c65
Elena-Alvarez-Campos <elenaalvarezcampos8@gmail.com>
2026-03-12 17:41:14 +0100

    Pequeños avances

--- BibliotecaHerencia/src/Libro.java
+++ BibliotecaHerencia/src/Libro.java
@@ -1,2 +1,9 @@
-public class Libro {
+public abstract class Libro extends ItemBiblioteca {
+    //Attributes
+    private String autor;
+    //COnstructor
+    public Libro(String autor,String id, String titulo, boolean estadoPrestado){
+        super(String id, String titulo, boolean estadoPrestado);
+        this.autor=autor;
+    }
 }
